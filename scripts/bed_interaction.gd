bfbaa59284789dd3445663b50cdbebe01f1def40
Jan Vais <honzavais.007@gmail.com>
2025-02-14 18:19:44 +0100

    Initial commit

---
+++ scripts/bed_interaction.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var interact: bool = false
+
+func _on_area_entered(area):
+	if area.is_in_group("Player"):
+		interact = true
+		
+func _on_area_exited(area):
+	interact = false
+		
+func _input(event):
+	if interact and Input.is_action_just_pressed("interact"):
+		get_tree().change_scene_to_file("res://scenes/locations/dream_room.tscn")
---
+++ scripts/jon.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var speed = 50
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+	var x_axis = Input.get_axis("left", "right")
+	var y_axis = Input.get_axis("up", "down")
+	var input_direction = Vector2(x_axis, y_axis)
+	print(input_direction)
+	if input_direction == Vector2(0, 0):
+		animated_sprite_2d.play("idle")
+	elif input_direction == Vector2(0, -1):
+		animated_sprite_2d.play("up")
+	elif input_direction == Vector2(0, 1):
+		animated_sprite_2d.play("down")
+	elif input_direction == Vector2(-1, 0):
+		animated_sprite_2d.play("left")
+	elif input_direction == Vector2(1, 0):
+		animated_sprite_2d.play("right")
+		
+	velocity = input_direction * speed
+	
+	move_and_slide()
