716e0f5faaa7f29557c546084e0382f44a1d8086
Jan Vais <honzavais.007@gmail.com>
2025-02-25 14:20:04 +0100

    Happy Dream Added

---
+++ scripts/giggly_interaction.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var interact: bool = false
+
+func _on_area_entered(area):
+	if area.is_in_group("Player"):
+		interact = true
+		
+func _on_area_exited(_area):
+	interact = false
+		
+func _input(_event):
+	if interact and Input.is_action_just_pressed("interact"):
+		get_tree().change_scene_to_file("res://scenes/locations/nexus.tscn")
