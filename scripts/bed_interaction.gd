faac0b4a1fdb297289707a96fab32cc1ea6d121e
Jan Vais <honzavais.007@gmail.com>
2025-03-09 16:41:43 +0100

    Transition Animation Not Working Properly

--- scripts/bed_interaction.gd
+++ scripts/bed_interaction.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 var interact: bool = false
+@onready var canvas_layer = $CanvasLayer
+@onready var animated_sprite_2d = $CanvasLayer/AnimatedSprite2D2
 
 func _on_area_entered(area):
 	if area.is_in_group("Player"):
@@ -11,4 +13,9 @@ func _on_area_exited(_area):
 		
 func _input(_event):
 	if interact and Input.is_action_just_pressed("interact"):
+		canvas_layer.visible = true
+		animated_sprite_2d.play("default")
+		await animated_sprite_2d.animation_finished
+		animated_sprite_2d.play("second")
+		canvas_layer.visible = false
 		get_tree().change_scene_to_file("res://scenes/locations/dream_room.tscn")
