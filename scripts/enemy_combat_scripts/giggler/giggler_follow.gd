878629586b4313733077c4cc2c9c354148a6a53c
Jan Vais <honzavais.007@gmail.com>
2025-04-14 12:02:52 +0200

    Enemy Radius Added

--- scripts/enemy_combat_scripts/giggler/giggler_follow.gd
+++ scripts/enemy_combat_scripts/giggler/giggler_follow.gd
@@ -1,14 +1,25 @@
 extends CharacterBody2D
 
 @onready var player = $"../CharacterBody2D"
+@onready var enemy_radius = $EnemyRadius
 var speed = 25
+var interact: bool = false
 
-func _ready():
-	pass
+func _on_enemy_radius_area_entered(area):
+	if area.is_in_group("Player"):
+		print("entered")
+		interact = true
+
+func _on_enemy_radius_area_exited(area):
+	interact = false
+	print("exited")
 
 func _physics_process(_delta):
 	var direction = (player.position-position).normalized()
-	velocity = direction * speed
+	if interact:
+		velocity = direction * speed
+	if !interact:
+		velocity = direction * 0
 	if !GlobalVariables.in_combat:
 		move_and_slide()
 
