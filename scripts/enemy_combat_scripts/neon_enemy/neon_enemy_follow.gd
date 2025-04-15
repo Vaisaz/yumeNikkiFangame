4a876dbd9923d3874125af25eb7a200fa8e6cfa3
Jan Vais <honzavais.007@gmail.com>
2025-04-15 14:34:51 +0200

    Neon Enemy Added + Happy Dream Update

---
+++ scripts/enemy_combat_scripts/neon_enemy/neon_enemy_follow.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@onready var player = $"../CharacterBody2D"
+@onready var enemy_radius = $EnemyRadius
+var speed = 50
+var interact: bool = false
+
+func _on_enemy_radius_area_entered(area):
+	if area.is_in_group("Player"):
+		print("entered")
+		interact = true
+
+func _on_enemy_radius_area_exited(area):
+	interact = false
+	print("exited")
+
+func _physics_process(_delta):
+	var direction = (player.position-position).normalized()
+	if interact:
+		velocity = direction * speed
+	if !interact:
+		velocity = direction * 0
+	if !GlobalVariables.in_combat:
+		move_and_slide()
---
+++ scripts/interaction_scripts/neon_enemy_interaction.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+@onready var interaction = $"."
+
+var player_max_health = Combat.player_max_health
+var player_current_health = Combat.player_current_health
+
+func _on_area_entered(area):
+	if area.is_in_group("Player"):
+		Inventory.add_coins = 2
+		Combat.add_xp = 5
+		GlobalVariables.in_combat = true
+		Transition.combat_transition_animation.visible = true
+		Transition.combat_transition_animation.play("default")
+		Transition.canvas_layer.visible = true
+		Transition.animated_sprite_2d.visible = false
+		await Transition.combat_transition_animation.animation_finished
+		Transition.combat_transition_animation.visible = false
+		Transition.canvas_layer.visible = false
+		Transition.animated_sprite_2d.visible = true
+		$"..".queue_free()
+		GlobalVariables.debounce = true
+		Combat.enemy_texture.texture = load("res://assets/combat/neon_enemy.png")
+		Combat.enemy_max_health = 25
+		Combat.enemy_current_health = 25
+		Combat.enemy_attack = 5
+		Combat.enemy_health(Combat.enemy_current_health, Combat.enemy_max_health)
+		Combat.player_health()
+		print("interacted")
+		Combat.combat_layer.visible = true
+		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE)
+
