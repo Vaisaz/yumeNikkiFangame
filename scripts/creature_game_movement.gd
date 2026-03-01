24a55310b6ec2342e3b66cfce36b7827323bd6c5
Jan Vais <honzavais.007@gmail.com>
2026-03-01 17:02:51 +0100

    Minigame Scene Added + Minigame Character Added

---
+++ scripts/creature_game_movement.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+const SPEED = 50.0
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if direction > 0:
+		animated_sprite.play("run")
+		animated_sprite.flip_h = true
+	elif direction < 0:
+		animated_sprite.play("run")
+		animated_sprite.flip_h = false
+	else:
+		animated_sprite.play("idle")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
